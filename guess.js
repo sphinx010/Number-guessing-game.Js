f744b4d5244ff71e82fb429081ed43517d4b4e92
Sphinx010 <paultreaker@gmail.com>
2023-06-30 17:40:43 +0100

    guessin-game

---
+++ guess.js
@@ -0,0 +1,30 @@
+let randomGen = document.getElementById('right-answer');
+let generateBtn = document.getElementById('generate-btn');
+let userInput = document.getElementById('user-number');
+let resultText = document.createElement('p');
+
+document.body.appendChild(resultText);
+
+function crossCheck() {
+  let generatedNumber = parseInt(randomGen.value);
+  let userGuess = parseInt(userInput.value);
+
+  if (isNaN(userGuess)) {
+    resultText.textContent = 'Invalid input. Please enter a number.';
+  } else if (userGuess === generatedNumber) {
+    resultText.textContent = 'Congratulations! Your guess is correct.';
+  } else {
+    resultText.textContent = 'Oops! Your guess is incorrect. Try again.';
+  }
+}
+
+function generateRandomNumber(event) {
+  event.preventDefault(); // Prevents page reload
+  let logic4Random = Math.floor(Math.random() * 100) + 1;
+  randomGen.value = logic4Random;
+}
+
+generateBtn.addEventListener('click', generateRandomNumber);
+resultText.style.margin ='75px ';
+document.getElementById('generate-btn').addEventListener('click', crossCheck);
+
